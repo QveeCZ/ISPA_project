0b0ac6fb38f83155b4b142fe470359551b2ea8b9
Matty <yolomonium@gmail.com>
2017-11-19 15:05:29 +0100

    JSONImport update

--- src/IOBundle/Util/JSONImport.php
+++ src/IOBundle/Util/JSONImport.php
@@ -8,9 +8,24 @@ use SchoolBundle\Entity\Car;
 use SchoolBundle\Entity\Lector;
 use SchoolBundle\Entity\School;
 use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\ORM\EntityManager;
 
 class JSONImport extends BaseImport
 {
+
+    /**
+     * @var EntityManager $em
+     */
+    protected $em;
+
+    /**
+     * SystemExport constructor.
+     * @param EntityManager $em
+     */
+    public function __construct($em)
+    {
+        $this->em = $em;
+    }
     /**
      * @param File $file
      * @param School $school
@@ -33,20 +48,25 @@ class JSONImport extends BaseImport
                 $lector->setName($row['name']);
                 $lector->setSurname($row['surname']);
                 $lector->setEmail($row['email']);
-                $lector->setDateMedical($row['dateMedical']);
+                //$lector->setDateMedical($row['dateMedical']);
+                $date = new \DateTime($row['dateMedical']);
+                $lector->setDateMedical($date);
                 $lector->setPhone($row['phone']);
                 $lector->setSchool($school);
 
-                $school->addSchoolLectors($lector);
+                $this->em->persist($lector);
+                $this->em->flush();
             }else if($row['spz']!=null) {
                 $car = new Car();
                 $car->setSchool($school);
                 $car->setSpz($row['spz']);
                 $car->setColor($row['color']);
                 $car->setCondition($row['condition']);
-                $car->setDateSTK($row['dateSTK']);
+                $date = new \DateTime($row['dateSTK']);
+                $car->setDateSTK($date);
 
-                $school->addSchoolCars($car);
+                $this->em->persist($car);
+                $this->em->flush();
             }
 
 
